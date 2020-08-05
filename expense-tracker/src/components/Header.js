f457073381fe97294c439645d31d542e89d0176b
Suryakumar Arumugam <asdsurya@gmail.com>
2020-08-05 14:08:56 +0530

    Implemented folder structure and styles

---
+++ expense-tracker/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export const Header = props => {
+    return (
+        <h2>
+            Expense Tracker
+        </h2>
+    )
+}
+
+
+
